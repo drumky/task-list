0d1e6e68c145829cc6575721e43e83ffc784d9eb
drumky <axeljcob@gmail.com>
2020-03-31 11:54:11 +0800

    Functionality update: Filtering tasks, removing tasks and clearing all tasks

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ const taskInput = document.querySelector("#task");
 loadEventListeners();
 
 function loadEventListeners() {
+  //Add Task event
   form.addEventListener("submit", addTask);
+  //Remove single task event
+  taskList.addEventListener("click", removeTask);
+  //Clear all tasks event
+  clearBtn.addEventListener("click", clearTasks);
+  //Filter tasks events
+  filter.addEventListener("keyup", filterTasks);
 }
 
 //add task
@@ -35,9 +42,41 @@ function addTask(e) {
   // Append the link to li
   li.appendChild(link);
 
-  //Append li to ul
-  taskList.append(li);
-  console.log(li);
+  // Append li to ul
+  taskList.appendChild(li);
+  // console.log(li);
+
+  // Clear input
+  taskInput.value = "";
 
   e.preventDefault();
+}
+
+//Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear tasks
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
