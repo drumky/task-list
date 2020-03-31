d4733eba23407ea6860c6c979e9366d8df55e4c9
drumky <axeljcob@gmail.com>
2020-03-31 09:52:29 +0800

    defined UI Vars

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("connected!");
+
+// define UI vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
